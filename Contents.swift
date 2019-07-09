09b3d30c27bfe47d01fe9301533330aa8d240afc
Shalini Kishore <shaliniskishore@gmail.com>
2019-07-09 09:49:47 -0700

    Second commit

---
+++ Contents.swift
@@ -0,0 +1,14 @@
+//import UIKit
+////Intention: To check if a number is even or odd.
+//func numberCheck( number :Int) {
+//    if( number % 2 == 0){
+//        print("even")
+//    }
+//    else { //If statement for odd numbers
+//
+//print("odd")
+//}
+//}
+//numberCheck(number :5)
+
+
